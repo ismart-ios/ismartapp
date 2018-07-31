e7f77817bacb5a873e27ba9a35145a5eec59edd0
mac <mac@sitvmac.local>
2018-07-31 13:44:31 +0800

    123

---
+++ Food.swift
@@ -0,0 +1,23 @@
+//
+//  Food.swift
+//  shopapp
+//
+//  Created by mac on 2018/7/26.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+import Foundation
+
+class Food {
+    var name = ""
+    var type = ""
+    var location = ""
+    var image = ""
+
+init(name:String, type:String, location:String,image:String){
+    self.name = name
+    self.type = type
+    self.location = location
+    self.image = image
+    }
+}
--- shopapp/Controller/shopappViewController.swift
+++ shopapp/Controller/shopappViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  shopappViewController.swift
 //  shopapp
 //
 //  Created by mac on 2018/7/16.
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class shopappViewController: UIViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
